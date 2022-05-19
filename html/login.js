1cd70ec1ce7817f79190bd153859c9837125b774
Mark <you@example.com>
2022-05-18 20:02:49 -0500

    commit

--- html/login.js
+++ html/login.js
@@ -2,7 +2,7 @@ var attempt = 3;
 function validate(){
     var usuar=document.getElementById("username").value;
     var password=document.getElementById("password").value;
-    if(username=="Admin" && password=="passwordsmarthome"){
+    if(usuar=="Admin" && password=="passwordsmarthome"){
         alert("Ingreso Exitosamente!");
         window.location="index2.html";
         return false;
@@ -10,7 +10,7 @@ function validate(){
     else{
         attempt--;
     }
-    alert("Intentos restantes:"+attempt)
+    alert("Intentos restantes:"+attempt+".")
     if(attempt==0){
         document.getElementById("username").disable=True;
         document.getElementById("password").disable=True;
